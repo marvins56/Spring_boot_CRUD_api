6cb90f46a7a456fb9f43ef3dddaf5609a8c3ac34
marvins56 <82571414+marvins56@users.noreply.github.com>
2023-01-29 17:46:54 +0300

    Exception not found class

---
+++ src/main/java/com/example/crudapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,36 @@
+package com.example.crudapi.exception;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String resourceName;
+	private String FieldName;
+	private Object FieldValue;
+	
+	public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+		super(String.format("%s not found with %s : '%s'",resourceName, fieldName,fieldValue));
+		this.resourceName = resourceName;
+		FieldName = fieldName;
+		FieldValue = fieldValue;
+	}
+
+
+	public String getResourceName() {
+		return resourceName;
+	}
+
+	public String getFieldName() {
+		return FieldName;
+	}
+
+	
+	public Object getFieldValue() {
+		return FieldValue;
+	}
+
+	
+	
+}
