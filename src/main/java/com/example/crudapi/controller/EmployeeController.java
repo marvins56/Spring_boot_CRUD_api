d900209f620584c754018b89aa84a485f5f31788
marvins56 <82571414+marvins56@users.noreply.github.com>
2023-01-30 22:07:27 +0300

    save employee  rest api

--- src/main/java/com/example/crudapi/controller/EmployeeController.java
+++ src/main/java/com/example/crudapi/controller/EmployeeController.java
@@ -1,7 +1,13 @@
 package com.example.crudapi.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.crudapi.model.Employee;
 import com.example.crudapi.service.EmployeeService;
 
 @RestController
@@ -9,10 +15,20 @@ public class EmployeeController {
 
     private EmployeeService employeeService;
 
-	public EmployeeController(EmployeeService employeeService) {
-		super();
-		this.employeeService = employeeService;
-	}
+    public EmployeeController(EmployeeService employeeService) {
+        super();
+        this.employeeService = employeeService;
+    }
+    
+    //building createt rest api
+    //post mapping cause it handles post http requests
+    @PostMapping
+public ResponseEntity<Employee> SaveEmployee(@RequestBody Employee employee){
+   
+    return new ResponseEntity<Employee>(employeeService.SaveEmployee(employee), HttpStatus.CREATED);
+
+}
+
 
     
 	
